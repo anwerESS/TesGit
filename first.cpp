1889cc80bc3a7d0f64cf283ec74dee75b4d42268
anwerESS <chlayfa.anouar@gmail.com>
2019-02-02 15:02:59 -0800

    add class first

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include "first.h"
+
+
+
+first::first()
+{
+}
+
+
+first::~first()
+{
+}
+
+void first::printHello1First()
+{
+	std::cout << "hello1 from first class";
+}
+//
+
---
+++ first.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+class first
+{
+public:
+	first();
+	~first();
+	void printHello1First();
+};
+
